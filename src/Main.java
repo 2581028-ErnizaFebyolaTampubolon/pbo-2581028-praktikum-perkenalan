f987118233a808f2dc7212ed268ab1b17585c393
Erniza Febyola Tampubolon <2581028@unai.edu>
2026-09-11 10:19:21 +0700

    menambahkan umur dan tinggi

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,6 @@ public class Main {
         double tinggi = scanner.nextDouble();
         scanner.nextLine();
 
-        System.out.print("Masukan asal kota : ");
-        String asalkota = scanner.nextLine();
 
-        System.out.println(
-                "Hallo! Nama saya " + nama + " (NIM " + nim + ")" + ", umur " + umur + " tahun, tinggi " + tinggi + " cm, berasal dari " + asalkota + ".");
-        scanner.close();
     }
 }
